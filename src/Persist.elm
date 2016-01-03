05a739d02f1869845df53e3fbc4ae1a5cff7ab35
Isaac Shapira <fresheyeball@gmail.com>
2016-01-03 16:07:20 -0700

    parse checks successful

--- src/Persist.elm
+++ src/Persist.elm
@@ -1,4 +1,4 @@
-module Persist (save, restore) where
+module Persist (save, restore, encodeFeedings, decodeFeedings) where
 
 import LocalStorage
 import Types exposing (..)
@@ -14,71 +14,86 @@ key =
     "whichTitWhen"
 
 
+
+-- Encode
+
+
+encodeFeeding : Feeding -> Encode.Value
+encodeFeeding ( time, lactation ) =
+    Encode.object
+        [ ( "lactation", Encode.string (toString lactation) )
+        , ( "time", Encode.float time )
+        ]
+
+
+encodeFeedings : List Feeding -> String
+encodeFeedings feedings =
+    Encode.list (List.map encodeFeeding feedings)
+        |> Encode.encode 0
+
+
+
+-- Decode
+
+
+decodeFeeding : Json.Decode.Decoder Feeding
+decodeFeeding =
+    let
+        decodeTup =
+            Json.Decode.object2
+                (,)
+                ("time" := Json.Decode.float)
+                ("lactation" := Json.Decode.string)
+
+        decodeLactation ( time, lactationString ) =
+            case lactationString of
+                "LeftBreast" ->
+                    Ok ( time, LeftBreast )
+
+                "RightBreast" ->
+                    Ok ( time, RightBreast )
+
+                "Bottle" ->
+                    Ok ( time, Bottle )
+
+                "Done" ->
+                    Ok ( time, Done )
+
+                _ ->
+                    Err "Lactation Decode Failed"
+    in
+        Json.Decode.customDecoder
+            decodeTup
+            decodeLactation
+
+
+decodeFeedings : String -> Result String (List Feeding)
+decodeFeedings =
+    Json.Decode.list decodeFeeding
+        |> Json.Decode.decodeString
+
+
+
+-- Save Restore
+
+
 save : List Feeding -> Effects Action
 save feedings =
-    let
-        (=>) = (,)
-
-        feedings' =
-            let
-                encodeFeeding ( time, lactation ) =
-                    Encode.object
-                        [ "lactation" => Encode.string (toString lactation)
-                        , "time" => Encode.float time
-                        ]
-            in
-                Encode.list (List.map encodeFeeding feedings)
-                    |> Encode.encode 0
-    in
-        LocalStorage.set key feedings'
-            |> Task.map (always NoOp)
-            |> Effects.task
+    LocalStorage.set key (encodeFeedings feedings)
+        |> Task.map (always NoOp)
+        |> Effects.task
 
 
 restore : Effects Action
 restore =
-    let
-        decodeFeeding =
-            let
-                decodeTup =
-                    Json.Decode.object2
-                        (,)
-                        ("time" := Json.Decode.float)
-                        ("lactation" := Json.Decode.string)
+    Task.toResult (LocalStorage.get key)
+        |> Task.map
+            (\result ->
+                case result `Result.andThen` decodeFeedings of
+                    Ok feedings ->
+                        Restore feedings
 
-                decodeLactation ( time, lactationString ) =
-                    case lactationString of
-                        "LeftBreast" ->
-                            Ok ( time, LeftBreast )
-
-                        "RightBreast" ->
-                            Ok ( time, RightBreast )
-
-                        "Bottle" ->
-                            Ok ( time, Bottle )
-
-                        "Done" ->
-                            Ok ( time, Done )
-
-                        _ ->
-                            Err "Lactation Decode Failed"
-            in
-                Json.Decode.customDecoder
-                    decodeTup
-                    decodeLactation
-
-        decodeModel =
-            Json.Decode.list decodeFeeding
-                |> Json.Decode.decodeString
-    in
-        Task.toResult (LocalStorage.get key)
-            |> Task.map
-                (\result ->
-                    case result `Result.andThen` decodeModel of
-                        Ok feedings ->
-                            Restore feedings
-
-                        _ ->
-                            NoOp
-                )
-            |> Effects.task
+                    _ ->
+                        NoOp
+            )
+        |> Effects.task
